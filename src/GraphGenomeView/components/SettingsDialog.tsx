264620de318416168a75e200c6f35f9e44c4fc06
Colin <colin.diesh@gmail.com>
2025-02-12 12:19:59 -0500

    MUI tab

--- src/GraphGenomeView/components/SettingsDialog.tsx
+++ src/GraphGenomeView/components/SettingsDialog.tsx
@@ -9,7 +9,11 @@ import {
   FormControlLabel,
   Radio,
   RadioGroup,
+  Slider,
+  Tab,
+  Tabs,
   TextField,
+  Typography,
 } from '@mui/material'
 import { observer } from 'mobx-react'
 
@@ -23,6 +27,7 @@ const SettingsDialog = observer(function ({
   handleClose: () => void
 }) {
   const { mode, graphSettings, serverRoot } = model
+  const [tabValue, setTabValue] = useState(0)
   const {
     chunkSize,
     strengthCenter,
@@ -30,164 +35,168 @@ const SettingsDialog = observer(function ({
     linkSteps,
     sequenceThickness,
   } = graphSettings
-  const [tempChunkSize, setTempChunkSize] = useState(`${chunkSize}`)
-  const [tempLinkSteps, setTempLinkSteps] = useState(`${linkSteps}`)
-  const [tempStrengthCenter, setTempStrengthCenter] = useState(
-    `${strengthCenter}`,
-  )
-  const [tempSequenceThickness, setTempSequenceThickness] = useState(
-    `${sequenceThickness}`,
-  )
-  const [tempLinkThickness, setTempLinkThickness] = useState(`${linkThickness}`)
+  const [tmpChunkSize, setTmpChunkSize] = useState(`${chunkSize}`)
+  const [tmpLinkSteps, setTmpLinkSteps] = useState(linkSteps)
+  const [tmpStrengthCenter, setTmpStrengthCenter] = useState(strengthCenter)
+  const [tmpSequenceThickness, setTmpSequenceThickness] =
+    useState(sequenceThickness)
+  const [tmpLinkThickness, setTmpLinkThickness] = useState(linkThickness)
   return (
     <Dialog open maxWidth="xl" title="Settings" onClose={handleClose}>
-      <DialogContent style={{ minWidth: 800 }}>
-        <FormControl component="fieldset">
-          <RadioGroup
-            value={mode}
-            onChange={event => {
-              model.setMode(event.target.value)
-            }}
-          >
-            <FormControlLabel
-              value="files"
-              control={<Radio />}
-              label="Use GFA files"
-            />
-            <FormControlLabel
-              value="server"
-              control={<Radio />}
-              label="Use GFA server"
-            />
-          </RadioGroup>
-        </FormControl>
-        {mode === 'server' ? (
-          <div>
-            <div>Server settings:</div>
-            <TextField
-              label="Path to the simpleGfaServer e.g. http://localhost:9000/"
-              style={{ minWidth: 500 }}
-              value={serverRoot}
-              onChange={event => model.setServerRoot(event.target.value)}
-            />
-          </div>
-        ) : null}
+      <DialogContent style={{ maxWidth: 500 }}>
+        <Tabs
+          value={tabValue}
+          onChange={(_event, newValue) => setTabValue(newValue)}
+          sx={{ borderBottom: 1, borderColor: 'divider', mb: 2 }}
+        >
+          <Tab label="Mode Settings" />
+          <Tab label="Graph Settings" />
+        </Tabs>
 
-        <div>
-          <div>
-            Number of simulation steps for the links
-            <br />
+        {tabValue === 0 ? (
+          <>
+            <FormControl component="fieldset">
+              <RadioGroup
+                value={mode}
+                onChange={event => {
+                  model.setMode(event.target.value)
+                }}
+              >
+                <FormControlLabel
+                  value="files"
+                  control={<Radio />}
+                  label="Use GFA files"
+                />
+                <FormControlLabel
+                  value="server"
+                  control={<Radio />}
+                  label="Use GFA server"
+                />
+              </RadioGroup>
+            </FormControl>
+            {mode === 'server' ? (
+              <div>
+                <div>Server settings:</div>
+                <TextField
+                  label="Path to the simpleGfaServer e.g. http://localhost:9000/"
+                  style={{ minWidth: 500 }}
+                  value={serverRoot}
+                  onChange={event => model.setServerRoot(event.target.value)}
+                />
+              </div>
+            ) : null}
+          </>
+        ) : (
+          <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
             <div>
-              Increases the rigidity of the simulation, higher numbers e.g. 10
-              makes things much less floppy
+              <div>
+                <Typography variant="h6">
+                  Number of simulation steps for the links
+                </Typography>
+                <br />
+                <div>
+                  Increases the rigidity of the simulation, higher numbers e.g.
+                  10 makes things much less floppy
+                </div>
+              </div>
+              <div>
+                <Slider
+                  min={1}
+                  max={20}
+                  value={+tmpLinkSteps}
+                  onChange={(_event, val) => {
+                    setTmpLinkSteps(val as number)
+                  }}
+                />
+                Current value: {linkSteps}
+              </div>
+            </div>
+            <div>
+              <div>
+                <Typography variant="h6">Sequence chunk size</Typography>
+                <br />
+                <div>
+                  If a contig is of length 5000, then chunk length 1000 would
+                  become 5 segments. Note: contigs smaller than the chunk length
+                  may be less proportionally sized (they do not get
+                  spaghettified)
+                </div>
+              </div>
+              <div>
+                <TextField
+                  value={tmpChunkSize}
+                  onChange={event => {
+                    setTmpChunkSize(event.target.value)
+                  }}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <Typography variant="h6">Strength (particle charge)</Typography>
+                <br />
+                <div>
+                  Akin to charged particle force, a large negative number
+                  increases the repulsive force
+                </div>
+              </div>
+              <div>
+                <Slider
+                  min={-100}
+                  max={-1}
+                  value={tmpStrengthCenter}
+                  onChange={(_event, val) => {
+                    setTmpStrengthCenter(val as number)
+                  }}
+                />
+                Current value: {strengthCenter}
+              </div>
+            </div>
+            <div>
+              <div>
+                <Typography variant="h6">Sequence thickness (px)</Typography>
+              </div>
+              <div>
+                <Slider
+                  min={1}
+                  max={20}
+                  value={tmpSequenceThickness}
+                  onChange={(_event, val) => {
+                    setTmpSequenceThickness(val as number)
+                  }}
+                />
+                Current value: {sequenceThickness}px
+              </div>
+            </div>
+            <div>
+              <div>
+                <Typography variant="h6">Link thickness (px)</Typography>
+              </div>
+              <div>
+                <Slider
+                  min={1}
+                  max={20}
+                  value={tmpLinkThickness}
+                  onChange={(_event, val) => {
+                    setTmpLinkThickness(val as number)
+                  }}
+                />
+                Current value: {linkThickness}px
+              </div>
             </div>
           </div>
-          <div>
-            <input
-              type="range"
-              min={1}
-              max={20}
-              style={{ width: '100%' }}
-              value={tempLinkSteps}
-              onChange={event => {
-                setTempLinkSteps(event.target.value)
-              }}
-            />
-            Current value: {linkSteps}
-          </div>
-        </div>
-        <div>
-          <div>
-            Sequence chunk size
-            <br />
-            <div>
-              If a contig is of length 5000, then chunk length 1000 would become
-              5 segments. Note: contigs smaller than the chunk length may be
-              less proportionally sized (they do not get spaghettified)
-            </div>
-          </div>
-          <div>
-            <TextField
-              value={tempChunkSize}
-              onChange={event => {
-                setTempChunkSize(event.target.value)
-              }}
-            />
-          </div>
-        </div>
-        <div>
-          <div>
-            Strength (particle charge)
-            <br />
-            <div>
-              Akin to charged particle force, a large negative number increases
-              the repulsive force
-            </div>
-          </div>
-          <div>
-            <input
-              type="range"
-              min={-100}
-              max={-1}
-              style={{ width: '100%' }}
-              value={tempStrengthCenter}
-              onChange={event => {
-                setTempStrengthCenter(event.target.value)
-              }}
-            />
-            Current value: {strengthCenter}
-          </div>
-        </div>
-        <div>
-          <div>
-            Sequence thickness
-            <br />
-            <div>Visual thickness in px for the sequence chunks</div>
-          </div>
-          <div>
-            <input
-              type="range"
-              min={1}
-              max={20}
-              style={{ width: '100%' }}
-              value={tempSequenceThickness}
-              onChange={event => {
-                setTempSequenceThickness(event.target.value)
-              }}
-            />
-            Current value: {sequenceThickness}px
-          </div>
-        </div>
-        <div>
-          <div>
-            Link thickness
-            <br />
-            <div>Visual thickness in px for the links</div>
-          </div>
-          <div>
-            <input
-              type="range"
-              min={1}
-              max={20}
-              style={{ width: '100%' }}
-              value={tempLinkThickness}
-              onChange={event => {
-                setTempLinkThickness(event.target.value)
-              }}
-            />
-            Current value: {linkThickness}px
-          </div>
-        </div>
+        )}
       </DialogContent>
       <DialogActions>
         <Button
           variant="contained"
           onClick={() => {
-            graphSettings.setChunkSize(+tempChunkSize)
-            graphSettings.setLinkSteps(+tempLinkSteps)
-            graphSettings.setStrengthCenter(+tempStrengthCenter)
-            graphSettings.setSequenceThickness(+tempSequenceThickness)
-            graphSettings.setLinkThickness(+tempLinkThickness)
+            graphSettings.setChunkSize(+tmpChunkSize)
+            graphSettings.setLinkSteps(+tmpLinkSteps)
+            graphSettings.setStrengthCenter(+tmpStrengthCenter)
+            graphSettings.setSequenceThickness(+tmpSequenceThickness)
+            graphSettings.setLinkThickness(+tmpLinkThickness)
             handleClose()
           }}
         >
