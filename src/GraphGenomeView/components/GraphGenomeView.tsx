342bf34e97e7b693956f631fc23bb1fb0846395e
Colin <colin.diesh@gmail.com>
2025-02-14 18:11:12 -0500

    Remove resizehandle

--- src/GraphGenomeView/components/GraphGenomeView.tsx
+++ src/GraphGenomeView/components/GraphGenomeView.tsx
@@ -1,30 +1,19 @@
 import React from 'react'
 
-import { ErrorMessage, ResizeHandle } from '@jbrowse/core/ui'
+import { ErrorMessage } from '@jbrowse/core/ui'
 import { observer } from 'mobx-react'
-import { makeStyles } from 'tss-react/mui'
 
 import GraphPanel from './GraphPanel'
 import Header from './Header'
 
 import type { GraphGenomeViewModel } from '../model'
 
-const useStyles = makeStyles()({
-  resizeHandle: {
-    height: 4,
-    background: '#ccc',
-    boxSizing: 'border-box',
-    borderTop: '1px solid #fafafa',
-    zIndex: 100,
-  },
-})
 const GraphGenomeView = observer(function ({
   model,
 }: {
   model: GraphGenomeViewModel
 }) {
-  const { height, error, graph } = model
-  const { classes } = useStyles()
+  const { error } = model
   return (
     <div>
       <div
@@ -38,12 +27,6 @@ const GraphGenomeView = observer(function ({
           <GraphPanel model={model} />
         </div>
       </div>
-      {graph ? (
-        <ResizeHandle
-          onDrag={n => model.setHeight(height + n)}
-          className={classes.resizeHandle}
-        />
-      ) : null}
     </div>
   )
 })
